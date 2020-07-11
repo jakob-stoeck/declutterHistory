6e09ffd3d7f8f01e8863f7a4f8fa9e43bad4bb03
Jakob Stoeck <jakob.stoeck@grabtaxi.com>
2020-07-11 22:34:07 +0800

    added trie

--- background.js
+++ background.js
@@ -2,11 +2,10 @@ function createUrlList(text) {
   const urls = text.split("\n").filter(url => {
     return !(url == '' || url.startsWith('//'));
   });
-  console.debug(urls);
   return urls;
 }
 
-function contains(url, urls) {
+function listContains(url, urls) {
   for (const u of urls) {
     if (url.startsWith(u)) {
       return true;
@@ -15,14 +14,65 @@ function contains(url, urls) {
   return false;
 }
 
+function createUrlTrie(text) {
+  const root = {};
+  let curNode = root;
+  for (c of text) {
+    if (typeof curNode[c] === 'undefined') {
+      curNode[c] = {};
+    }
+    curNode = c === "\n" ? root : curNode[c];
+  }
+  curNode["\n"] = {}; // needed in case of missing line feed at the end of text
+  return root;
+}
+
+function trieContains(url, trie) {
+  let curNode = trie;
+  for (c of url) {
+    if (typeof curNode[c] !== 'undefined') {
+      curNode = curNode[c];
+      if (curNode["\n"]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 // Doesn’t affect navigation. It seems there is no way to remove the current session "go back" items?
 function init(items) {
-  const urls = createUrlList(items.urls);
+  const urls = createUrlTrie(items.urls);
   chrome.history.onVisited.addListener(result => {
-    if (contains(result.url, urls)) {
+    if (trieContains(result.url, urls)) {
       chrome.history.deleteUrl({url: result.url});
     }
   });
 };
 
-chrome.storage.sync.get("urls", init);
+//chrome.storage.sync.get("urls", init);
+
+/*
+let t = `// One URL per line
+// Matches all URLs beginning with this
+// https://example.com/ matches all sub pages
+https://example.com/
+http://www.example.com`;
+
+function assert(condition, message) {
+  if (!condition) {
+    message = message || "Assertion failed";
+    if (typeof Error !== "undefined") {
+      throw new Error(message);
+    }
+    throw message; // Fallback
+  }
+}
+
+trie = createUrlTrie(t);
+assert(trieContains('https://example.com/asdf"', trie) === true);
+assert(trieContains('https://example.com/"', trie) === true);
+assert(trieContains('https://example.com"', trie) === false);
+assert(trieContains('http://www.example.com/asdf"', trie) === true);
+assert(trieContains('com/login.action?"', trie) === false);
+*/
